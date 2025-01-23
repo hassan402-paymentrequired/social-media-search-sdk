efcc3c8694adf33e8c3d9e81f79ee4bfe0c25523
codeWithMe <codewithhassan.404.not.found@gmail.com>
2025-01-23 09:48:19 +0100

    added linkedin search

--- test/facebookTest.php
+++ test/facebookTest.php
@@ -1,12 +1,13 @@
 <?php
 
 use Eaglewatch\Search\Facebook;
+use Eaglewatch\Search\LinkedIn;
 use Eaglewatch\Search\SnapChat;
 use Eaglewatch\Search\Thread;
 use Eaglewatch\Search\Tictok;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$response = (new SnapChat())->searchUserLenses('lateefhassa9035');
+$response = (new LinkedIn())->searchUsers('hassan');
 
 print_r($response);
